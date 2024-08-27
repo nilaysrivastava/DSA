6b1d0057fbd04119655f272423d815f8cb27363f
nilaysrivastava <nilay2103@gmail.com>
2024-08-27 22:51:53 +0530

    updates

--- Basic Maths/checkPalindrome.cpp
+++ Basic Maths/checkPalindrome.cpp
@@ -27,7 +27,6 @@ class Solution
 public:
     bool isPalindrome(int x)
     {
-        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
         if (x < 0 || (x % 10 == 0 && x != 0))
         {
             return false;
